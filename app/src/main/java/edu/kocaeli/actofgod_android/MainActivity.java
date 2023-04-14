6c680a679712306d5b1fe7fa6af8e92e2e4cd458
ahmettyavzz <ahmethamdiyavuzz@gmail.com>
2023-04-15 02:58:10 +0300

    add view binding

--- app/src/main/java/edu/kocaeli/actofgod_android/MainActivity.java
+++ app/src/main/java/edu/kocaeli/actofgod_android/MainActivity.java
@@ -1,13 +1,20 @@
 package edu.kocaeli.actofgod_android;
 
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import edu.kocaeli.actofgod_android.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }

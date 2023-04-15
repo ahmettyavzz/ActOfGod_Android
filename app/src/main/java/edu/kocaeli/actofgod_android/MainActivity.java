191509462c8444bc31a91277ca942849eb212fa1
ahmettyavzz <ahmethamdiyavuzz@gmail.com>
2023-04-15 04:08:26 +0300

    add button onClick event in MainActivity

--- app/src/main/java/edu/kocaeli/actofgod_android/MainActivity.java
+++ app/src/main/java/edu/kocaeli/actofgod_android/MainActivity.java
@@ -1,9 +1,10 @@
 package edu.kocaeli.actofgod_android;
 
-import android.view.View;
-import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 import edu.kocaeli.actofgod_android.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
     }
+
+    public void connect(View view) {
+//        String name=binding.editTextName.getText().toString();
+//
+//        Intent intent=new Intent(MainActivity.this,MapsActivity.class);
+//        startActivity(intent);
+        double latitude = 40.8010419;
+        double longitude = 29.9496113;
+        String uri = "geo:" + latitude + "," + longitude;
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        intent.setPackage("com.google.android.apps.maps");
+
+
+        startActivity(intent);
+    }
+
+
 }
